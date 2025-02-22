fcb31eb83cc1e18bfb1d84f8dd43421b2baade61
BarakFalach <barakf@wix.com>
2025-02-22 10:47:37 +0200

    remove notes

--- index.js
+++ index.js
@@ -2,8 +2,8 @@ import { mapExpenses } from './mapper/mapper.js';
 import { downloadExpenses } from './scraping/getCardData.js';
 
 const main = async () => {
-  // await downloadExpenses(false);
-  // await mapExpenses(false);
+  await downloadExpenses(false);
+  await mapExpenses(false);
   await downloadExpenses(true);
   await mapExpenses(true);
 

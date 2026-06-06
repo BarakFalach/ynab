85f76e829825d8af53aeef1b3cb434b85f75ec14
BarakFalach <barakf@wix.com>
2026-06-06 15:50:22 +0300

    align selectors

--- scraping/selectors.js
+++ scraping/selectors.js
@@ -1,17 +1,17 @@
 export const LOGIN_URL = 'https://www.max.co.il/login';
 
 export const SELECTORS = {
-  loginFormReady: 'span[_ngcontent-my-app-id-c171=""]',
-  passwordTab: 'span[_ngcontent-my-app-id-c171=""]',
+  loginFormReady: 'span[_ngcontent-my-app-id-c173=""]',
+  passwordTab: 'span[_ngcontent-my-app-id-c173=""]',
   usernameField: '[formcontrolname="username"]',
   passwordField: '[formcontrolname="password"]',
-  loginButton: 'button[_ngcontent-my-app-id-c166=""]',
+  loginButton: 'button[_ngcontent-my-app-id-c168=""]',
   dashboard: '.only-card-wrapper > :first-child',
   closePopup: '#close-popup',
   downloadExcel: 'span.download-excel',
 };
 
-const CARD_BASE = 'div.card.card-box.card-box-url.ng-star-inserted[_ngcontent-my-app-id-c129][appgtm]';
+const CARD_BASE = 'div.card.card-box.card-box-url.ng-star-inserted[_ngcontent-my-app-id-c130][appgtm]';
 
 export const getCardSelector = (isAdiCard) =>
   isAdiCard ? `${CARD_BASE}:nth-child(3)` : `${CARD_BASE}:nth-child(1)`;
